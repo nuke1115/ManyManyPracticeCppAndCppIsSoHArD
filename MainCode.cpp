ffe2f184ab167e558e4860cf105ec266d0ec2e6b
nuke1115 <nuclearbomb280@gmail.com>
2024-03-06 18:38:24 +0900

    asdf

--- MainCode.cpp
+++ MainCode.cpp
@@ -15,7 +15,7 @@ int main()
 	//printf("%d\n %f",teststruct.c, teststruct.d);
 
 	pointerBasic();
-
+	PrintPractice(10);
 
 	return 0;
 }
@@ -26,6 +26,8 @@ void pointerBasic()
 
 	int* b = NULL;
 
+	
+
 	b = &a;
 
 	printf("pointer %d\n original %d\n ", *b, a);
@@ -33,12 +35,14 @@ void pointerBasic()
 	*b = 100;
 
 	printf("after pointer %d\n", a);
-
-	delete b;
 }
 
 void PrintPractice(int a)
 {
 	printf("%d\n" , a);
-}
+	
+	std::cout << "Hello " << std::endl;
+	std::cout << "Hello " << a << std::endl;
 
+
+}
