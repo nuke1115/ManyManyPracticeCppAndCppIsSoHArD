7313f756f0d126619116c023985afcdf6270f40d
nuke1115 <nuclearbomb280@gmail.com>
2024-03-09 23:57:05 +0900

    문자열들

--- MainCode.cpp
+++ MainCode.cpp
@@ -7,16 +7,19 @@ void PrintPractice(int a);
 
 int main()
 {
+	
+
+
+	char a[8] = "1.aㅁ "; //한글 : 1칸에 3칸, 영어,문자,숫자 : 한칸에 1칸 //종료문자 한개의 공간은 무조건 확보해둬야된다
+	
+	const char * b = "asdf"; // 포인터로 해당 문자열이 있는 readonly영역을 참조해온다 // 따라서, 수정이 불가하고, const로 선언되야한다. //가장빠름
+
+	std::string c = "asdf"; // // //느릴수도 있음
+
 
 
 	return 0;
 }
-
-void TestPrint(int a, int b)
-{
-	printf("%d\n%d\n",a,b);
-}
-
 
 
 
