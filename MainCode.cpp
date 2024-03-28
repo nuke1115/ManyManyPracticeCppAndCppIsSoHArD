bcbb17695978bfffa6512645406cf4b1b24a43de
nuke1115 <nuclearbomb280@gmail.com>
2024-03-28 23:37:16 +0900

    test

--- MainCode.cpp
+++ MainCode.cpp
@@ -10,30 +10,8 @@ int main()
 	//char result[3]; 와 char result[3] = ""; 이것과의 차이
 	//strcat과 strcpy의 차이 찾기
 
-
-	int* arr = new int[3];
-
-
-	arr[0] = 1;
-	arr[1] = 2;
-	arr[2] = 3;
-
-
-	printf("%d %d %d" , arr[0], arr[1], arr[2]);
-
-	delete arr;
-
-	int* arr2 = new int[3];
-
-
-	arr2[0] = 1;
-	arr2[1] = 2;
-	arr2[2] = 3;
-
-
-	printf("%d %d %d", arr2[0], arr2[1], arr2[2]);
-
-	free(arr2);
+	//std::cout 과 printf() 의 차이
+	//스트림과 버퍼란?
 
 	return 0;
 }
