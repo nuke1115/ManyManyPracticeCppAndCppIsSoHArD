adb1d3ac48244f89ce5a23df8c0c6dd3d3ad3416
nuke1115 <nuclearbomb280@gmail.com>
2024-03-31 00:30:36 +0900

    test

--- MainCode.cpp
+++ MainCode.cpp
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 #include <iostream>
-#include <cstring>
+using namespace std;
 
 
 int main()
@@ -12,6 +11,31 @@ int main()
 
 	//std::cout 과 printf() 의 차이
 	//스트림과 버퍼란?
+	//그리디 알고리즘이란?
+	// & 와 addressof 의 차이
+
+	int a = 10;
+
+	int* aPtr = &a;
+
+	int* aPtr2 = addressof(a);
+
+
+	std::cin >> a;
+
+	std::cout << a <<endl;
+	
+	*aPtr = 20;
+
+	std::cout << a << endl;
+
+	*aPtr2 = 30;
+
+	std::cout << a << endl;
+
 
 	return 0;
 }
+
+//c++의 char 는 c#의 byte와 비슷함
+//c++에서는 문자,숫자 다 되면, c#에선 오로지 문자만 되고, 숫자는 byte로 나뉨
