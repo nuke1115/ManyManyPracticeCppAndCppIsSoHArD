acffcd9257ba54e27f2427c884f35fdf04f6d701
nuke1115 <nuclearbomb280@gmail.com>
2024-03-31 23:49:35 +0900

    test : upload

--- MainCode.cpp
+++ MainCode.cpp
@@ -9,30 +9,14 @@ int main()
 	//char result[3]; 와 char result[3] = ""; 이것과의 차이
 	//strcat과 strcpy의 차이 찾기
 
-	//std::cout 과 printf() 의 차이
+	//std::cout 과 printf() , puts() 의 차이
 	//스트림과 버퍼란?
 	//그리디 알고리즘이란?
 	// & 와 addressof 의 차이
-
-	int a = 10;
-
-	int* aPtr = &a;
-
-	int* aPtr2 = addressof(a);
-
-
-	std::cin >> a;
-
-	std::cout << a <<endl;
+	//배열의 시작점을 포인터로 지정하고, 그 포인터에 1씩 더하면, 배열의 다음 인덱스의 값을 찾을수 있다
+	//배열에 인덱스로 접근하는게 아니라, 포인터로 접근이 가능한가?
 	
-	*aPtr = 20;
-
-	std::cout << a << endl;
-
-	*aPtr2 = 30;
-
-	std::cout << a << endl;
-
+	
 
 	return 0;
 }
